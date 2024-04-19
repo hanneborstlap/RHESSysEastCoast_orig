1022ace117d669874f4c8b64111f7e8ff713226f
Hanne Borstlap <66225524+hanneborstlap@users.noreply.github.com>
2024-04-19 14:37:38 -0400

    Update rhessys.h

--- include/rhessys.h
+++ include/rhessys.h
@@ -1665,7 +1665,7 @@ struct patch_object
         //double  active_zone_z;
         struct  zone_object             *zone; /* parent zone *///<<------------------- not set
         double  grassIrrigation_m;
-	double irrigation_surf 
+	double irrigation_surf; 
         double  septicReleaseQ_m;
         double  sewerdrained; //<------- Spet 28 tracking how much is subsurface sewer drain
             double  sewerdrained_NO3;
