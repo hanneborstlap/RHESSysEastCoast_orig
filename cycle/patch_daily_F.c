cbebf6ac68503424380a2a197fdcd260836a0336
Hanne Borstlap <66225524+hanneborstlap@users.noreply.github.com>
2024-05-21 13:16:47 -0400

    Update patch_daily_F.c

--- cycle/patch_daily_F.c
+++ cycle/patch_daily_F.c
@@ -583,7 +583,7 @@ struct date createDateFromDateString(const char* dateString) {
     const char *patchID_filename = "inundation/inundation_patchID.txt";
 
     // Determine the number of records in the depth file
-    int max_values = countRecords(depth_filename);
+    int max_values = countValues(depth_filename);
     if (max_values < 0) {
         fprintf(stderr, "Failed to determine the number of records.\n");
         return 1;
