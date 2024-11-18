0832ec2e878422f383b3e26a645a1c9060ce0b2e
Hanne Borstlap <66225524+hanneborstlap@users.noreply.github.com>
2024-11-18 17:40:31 -0500

    Update output_patch.c

--- output/output_patch.c
+++ output/output_patch.c
@@ -176,7 +176,7 @@ void	output_patch(
 		patch[0].sat_def_pct_indexM * patch[0].soil_defaults[0][0].vksat_0zm[patch[0].sat_def_pct_index+1] 
 + (1.0-patch[0].sat_def_pct_indexM) * patch[0].soil_defaults[0][0].vksat_0zm[patch[0].sat_def_pct_index],
 		patch[0].soil_defaults[0][0].Ksat_0,
-		patch[0].infiltration;
+		patch[0].infiltration
                     );
 	// patch[0].soil_defaults[0][0].Ksat_0_v;
 	if (check <= 0) {
