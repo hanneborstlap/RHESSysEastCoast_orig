5774578d005f387af2cad7d1c3ae71e722418570
Hanne Borstlap <66225524+hanneborstlap@users.noreply.github.com>
2024-12-19 05:26:44 -0500

    Update construct_command_line.c

--- init/construct_command_line.c
+++ init/construct_command_line.c
@@ -104,7 +104,7 @@ struct	command_line_object	*construct_command_line(
 	command_line[0].version_flag = 0;
 	command_line[0].vsen[M] = 1.0;
 	command_line[0].vsen[K] = 1.0;
-	command_line[0].ditch_routing[R] = 1.0; 
+	command_line[0].ditch_routing = 1.0; 
     command_line[0].psen[M] = 1.0;
     command_line[0].psen[K] = 1.0;
 	command_line[0].sen[M] = 1.0;
@@ -446,7 +446,7 @@ struct	command_line_object	*construct_command_line(
 					exit(EXIT_FAILURE);
 				}/*end if*/
 			
-				command_line[0].ditch_routing[R] = (double)atof(main_argv[i]);
+				command_line[0].ditch_routing = (double)atof(main_argv[i]);
 				i++;
 			} /* end if */
 
