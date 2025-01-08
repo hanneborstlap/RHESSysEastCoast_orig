81e212ee4a465f267f004aa6b5ca1cf63c5531bf
Hanne Borstlap <66225524+hanneborstlap@users.noreply.github.com>
2025-01-08 15:34:41 -0500

    Update patch_daily_F.c

--- cycle/patch_daily_F.c
+++ cycle/patch_daily_F.c
@@ -546,7 +546,7 @@ void		patch_daily_F(
 	// START OF DITCHES 
 
     patch[0].ditch_routing = command_line[0].ditch_routing; 
-    patch[0].ditch_routing = command_line[0].ditch_routing*;
+    //patch[0].ditch_routing = command_line[0].ditch_routing*;
 
 
     if(command_line[0].ditchextraction_flag==1 && patch[0].drainage_type>0 && patch[0].drainage_type % actionDITCH==0){
