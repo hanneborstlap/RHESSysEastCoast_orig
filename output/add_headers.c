4bcd8f717dbdcedb668b7f6e9c22fef952e4d181
Hanne Borstlap <66225524+hanneborstlap@users.noreply.github.com>
2025-01-17 12:38:24 -0500

    Update add_headers.c

--- output/add_headers.c
+++ output/add_headers.c
@@ -161,7 +161,7 @@ void add_headers(struct world_output_file_object *world_output_files,
 	/*--------------------------------------------------------------*/
     ///<<<---------- here basin daily
 	outfile = world_output_files[0].basin[0].daily;
-	fprintf(outfile,"%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s\n" , // added 3 extra
+	fprintf(outfile,"%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s\n" , // added 3 extra
 		"day",
 		"month",
 		"year",
@@ -245,7 +245,8 @@ void add_headers(struct world_output_file_object *world_output_files,
         "unsat_fc",
         "rtz_fc",
 	"ditchflow",
-	"normalized_satdef"); // basin daily
+	"normalized_satdef",
+		"ditch_routing_param"); // basin daily
 
 	/*--------------------------------------------------------------*/
 	/*	Monthly							*/
